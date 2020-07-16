8da6d9a4f0aa707dd8607640ceef0d4f28cf0906
wangex <18810144150@126.com>
2020-07-16 19:21:43 +0800

    wex

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="login clearfix">
     <div class="login-main">
-      <div class="login-con" v-show="!isLogin">
+      <div class="login-left">
+        <div class="left-icon"></div>
+      </div>
+      <div class="login-con" v-show="isLogin">
         <div class="title">LOGIN</div>
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
+        <el-form :model="ruleFormLogin" :rules="loginLules" ref="ruleForm" class="demo-ruleForm">
           <el-form-item prop="username">
-            <el-input v-model="ruleForm.username"></el-input>
+            <el-input v-model="ruleFormLogin.username" placeholder="请输入账号"></el-input>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input type="password" v-model="ruleForm.password"></el-input>
+            <el-input type="password" v-model="ruleFormLogin.password" placeholder="请输入密码"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button>登录</el-button>
+            <el-button @click="$router.push('home')">登录</el-button>
           </el-form-item>
         </el-form>
         <div class="forget-pass">
-          <span>修改密码</span>
+          <span @click="isLogin = false">修改密码</span>
           <span></span>
-          <span>忘记密码</span>
+          <span @click="isLogin = false">忘记密码</span>
         </div>
       </div>
-      <div class="login-con forget-password" v-show="isLogin">
+      <div class="login-con forget-password" v-show="!isLogin">
         <div class="title forget-title">忘记密码</div>
         <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
           <el-form-item prop="username">
@@ -39,7 +42,11 @@
             <el-button>登录</el-button>
           </el-form-item>
         </el-form>
-        <div class="go-login">返回登录</div>
+        <div class="send-code">
+          <span></span>
+          <span>发送验证码</span>
+        </div>
+        <div class="go-login" @click="isLogin = true">返回登录</div>
       </div>
     </div>
   </div>
@@ -48,11 +55,23 @@
 export default {
   data() {
     return {
+      ruleFormLogin: {
+        username: '',
+        password: ''
+      },
       ruleForm: {
         username: '',
         password: '',
         code: '',
         passwordAgin: ''
+      },
+      loginLules: {
+        username: [
+          { required: true, message: '请输入账号', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
       },
       rules: {
         username: [
@@ -75,18 +94,25 @@ export default {
 </script>
 <style lang="stylus" scoped>
 .login
-  padding-top 126px
+  min-width 1334px
+  min-height 789px
+  background url('../../assets/images/login-bg.png') no-repeat
+  background-size auto 100%
+  // padding-top 126px
+  height 100vh
   .login-main
-    width 952px
+    width 100%
+    padding-top 80px
     height 580px
     margin 0 auto
-    background-color #fff
-    border-radius 10px
-    box-shadow 0px 4px 18px 0px rgba(0, 0, 0, 0.3)
+    // background-color #fff
+    // border-radius 10px
+    // box-shadow 0px 4px 18px 0px rgba(0, 0, 0, 0.3)
 
 .login-con
   width 430px
-  float right
+  float left
+  margin-left 380px
   padding-left 48px
   padding-top 84px
   .title
@@ -134,6 +160,7 @@ export default {
       margin 0 6px
 
 .forget-password
+  position relative
   .el-form
     /deep/ .el-form-item
       margin-bottom 16px
@@ -146,4 +173,32 @@ export default {
     color #1890ff
     text-align right
     cursor pointer
+  .send-code
+    position absolute
+    width 130px
+    right 78px
+    top 220px
+    span
+      float left
+    > span:first-child
+      display block
+      width 1px
+      height 36px
+      background-color #b9b9b9
+    > span:last-child
+      color #4891fe
+      font-size 18px
+      line-height 36px
+      margin-left 20px
+      cursor pointer 
+
+.login-left
+  width 67px
+  float left
+  margin-left 260px
+  padding-top 80px
+  .left-icon
+    width 67px
+    height 300px
+    background url('../../assets/images/login-left-icon.svg') no-repeat
 </style>
